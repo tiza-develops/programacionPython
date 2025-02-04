becad148a3ec8cd20d6691d7fcde8ecb040276e4
tiza <tiza@tuta.io>
2025-02-04 10:21:23 -0600

    Añadi la formula de Heron

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 class cuadro:
     def __init__ (self, lado):
         self.lado = lado
-        self.area = lado**2
         self.peri = lado*4
-        print(self.area)
+        self.area = lado**2
         print(self.peri)
+        print(self.area)
 
-cuadro(2)
+class rectangulo:
+    def __init__(self, l1, l2):
+        self.l1 = l1
+        self.l2 = l2
+        self.peri = l1*2+l2*2
+        self.area = l1*l2
+        print(self.peri)
+        print(self.area)
+
+class triangulo:
+    def __init__(self, l1,l2,l3):
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
+        self.semi = (l1+l2+l3)/2
+        self.peri = (l1+l2+l3)
+        self.area = (self.semi*(self.semi-self.l1)*(self.semi-self.l2)*(self.semi-self.l3)) ** 0.5
+        print(self.peri)
+        print(self.area)
