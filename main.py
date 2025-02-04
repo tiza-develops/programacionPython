d2975230d612e814e070507a6963a7f5d18d552d
tiza <tiza@tuta.io>
2025-02-04 09:55:14 -0600

    Primer tarea ayudantía En esta primer tarea creamos una clase para calcular el área y perímetro de un cuadro

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-class Circulo:
-    def __init__(self, radio):
-        self.radio = radio
-    def area(self): 
-        return 3.14*self.radio**2
+class cuadro:
+    def __init__ (self, lado):
+        self.lado = lado
+        self.area = lado**2
+        self.peri = lado*4
+        return "Perímetro"
 
-class Rectangulo:
-    def __init__(self, l1, l2):
-        self.l1 = l1
-        self.l2 = l2
-    def area(self):
-        return l1 * l2
-    def perimetro(self):
-        return (l1*2)+(l2*2)
-
-class Triangulo:
-    def __init__(self, l1, l2, l3):
-        self.l1 = l1
-        self.l2 = l2
-        self.l3 = l3
-
-class Pentagono:
-    def __init__(self, l1, l2, l3, l4, l5):
-        self.l1 = l1
-        self.l2 = l2
-        self.l3 = l3
-        self.l4 = l4
-        self.l5 = l5
+cuadro(4)
